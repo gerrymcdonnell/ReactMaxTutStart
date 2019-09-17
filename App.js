1b55a70421eca83a62b1c531e829c75b75d92fb0
Gerry Mc Donnell <gerrymcdonnell@users.noreply.github.com>
2019-09-17 20:03:54 +0100

    added input box and handler

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
 
 export default function App() {
+
+  state={
+    placeName:''
+  }
+
+  placeNameInputChangedHandler=(event)=>{
+    alert(event);
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      
+      <TextInput 
+      value={this.state.placeName}
+        onChangeText={this.placeNameInputChangedHandler}
+        style={
+        {width:300, borderColor:"black",borderWidth:1}
+        }
+      />
+
     </View>
   );
 }
